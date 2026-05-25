b6e7ed6156a9e683bb3b3adf2d101f85e067e4a7
hellennverenaa <hellen.verena20@gmail.com>
2026-05-25 10:11:27 -0300

    chore: setup inicial vue 3, tailwind v4 e vite

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// Aqui vai ficar a lógica do nosso portfólio depois
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-900 flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-5xl font-bold text-emerald-400 mb-4">
+        Infraestrutura Pronta! 🚀
+      </h1>
+      <p class="text-slate-300 text-xl">
+        O Tailwind CSS está configurado e rodando perfeitamente.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Não vamos usar CSS puro aqui, deixaremos tudo a cargo do Tailwind! */
+</style>
